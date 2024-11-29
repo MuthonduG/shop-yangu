76f52a9338b5fa53e27e21f64395a91508abacde
MuthonduG <muthondugithinji@gmail.com>
2024-11-29 13:21:19 +0300

    updates: toogle feature for the sidenav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,26 @@
-import type { Metadata } from "next";
+"use client";
+
+import React, { useState } from "react";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidenav from "@/components/Sidenav";
-
-export const metadata: Metadata = {
-  title: "Your App Title",
-  description: "Your app description",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isSideNavVisible, setIsSideNavVisible] = useState(true);
+
+  const toggleSidenav = () => {
+    setIsSideNavVisible((prev) => !prev);
+  };
+
   return (
     <html lang="en">
       <body className="antialiased">
-        <Navbar />
-        <Sidenav/>
+        <Navbar toggleSidenav={toggleSidenav} />
+        {isSideNavVisible && <Sidenav />}
         {children}
       </body>
     </html>
